21fd840999afdbf2179f7bd5945c682a09b78a43
divyack2 <divyack2@sp25-cs598a-08.cs.illinois.edu>
2025-04-10 21:27:58 -0500

    ideas for optimization

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 #include <sys/time.h>
 
+// TODO: add -O3 flag to Makefile
+
 float tdiff(struct timeval *start, struct timeval *end) {
   return (end->tv_sec-start->tv_sec) + 1e-6*(end->tv_usec-start->tv_usec);
 }
@@ -25,10 +27,11 @@ unsigned long long randomU64() {
   return seed;
 }
 
+// TODO: possibly find a faster way of computing a random number
 double randomDouble()
 {
    unsigned long long next = randomU64();
-   next >>= (64 - 26);
+   next >>= (64 - 26); // TODO: just...compute this?
    unsigned long long next2 = randomU64();
    next2 >>= (64 - 26);
    return ((next << 27) + next2) / (double)(1LL << 53);
@@ -40,7 +43,9 @@ double dt;
 double G;
 
 Planet* next(Planet* planets) {
+   // TODO: check if malloc is fast or if there's any faster alloc functions
    Planet* nextplanets = (Planet*)malloc(sizeof(Planet) * nplanets);
+   // TODO: parallelize this loop
    for (int i=0; i<nplanets; i++) {
       nextplanets[i].vx = planets[i].vx;
       nextplanets[i].vy = planets[i].vy;
@@ -50,18 +55,21 @@ Planet* next(Planet* planets) {
    }
 
    for (int i=0; i<nplanets; i++) {
+      // TODO: parallelize this loop?
       for (int j=0; j<nplanets; j++) {
          double dx = planets[j].x - planets[i].x;
          double dy = planets[j].y - planets[i].y;
          double distSqr = dx*dx + dy*dy + 0.0001;
          double invDist = planets[i].mass * planets[j].mass / sqrt(distSqr);
          double invDist3 = invDist * invDist * invDist;
+         // TODO: precompute the value for these
          nextplanets[i].vx += dt * dx * invDist3;
          nextplanets[i].vy += dt * dy * invDist3;
       }
       nextplanets[i].x += dt * nextplanets[i].vx;
       nextplanets[i].y += dt * nextplanets[i].vy;
    }
+   // TODO: maybe instead of malloc + free every time, malloc + clear?
    free(planets);
    return nextplanets;
 }
@@ -76,11 +84,15 @@ int main(int argc, const char** argv){
    dt = 0.001;
    G = 6.6743;
 
+   // TODO: check if malloc is fast or if there's any faster alloc functions
    Planet* planets = (Planet*)malloc(sizeof(Planet) * nplanets);
+   // TODO: don't keep recomputing randomDouble(), just compute once and store
    for (int i=0; i<nplanets; i++) {
       planets[i].mass = randomDouble() * 10 + 0.2;
+      // TODO: set x and y to be the same thing
       planets[i].x = ( randomDouble() - 0.5 ) * 100 * pow(1 + nplanets, 0.4);
       planets[i].y = ( randomDouble() - 0.5 ) * 100 * pow(1 + nplanets, 0.4);
+      // TODO: set vx and vy to be the same thing
       planets[i].vx = randomDouble() * 5 - 2.5;
       planets[i].vy = randomDouble() * 5 - 2.5;
    }
