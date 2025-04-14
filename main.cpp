1e0328e679957703b576a7c0cf06dce3b2615bd6
divyack2 <divyack2@sp25-cs598a-08.cs.illinois.edu>
2025-04-14 18:48:49 -0500

    trying out manual vectorization

--- main.cpp
+++ main.cpp
@@ -5,17 +5,11 @@
 #include <sys/time.h>
 #include <omp.h>
 
+#include <immintrin.h>
+
 float tdiff(struct timeval *start, struct timeval *end) {
   return (end->tv_sec-start->tv_sec) + 1e-6*(end->tv_usec-start->tv_usec);
 }
-
-struct Planet {
-   double mass;
-   double x;
-   double y;
-   double vx;
-   double vy;
-};
 
 struct PlanetArray{
    double *mass;
@@ -46,98 +40,78 @@ int timesteps;
 double dt;
 double G;
 
-// Planet* next(Planet* planets) {
-//    Planet* nextplanets = (Planet*)malloc(sizeof(Planet) * nplanets);
-//    #pragma omp parallel for simd
-//    for (int i=0; i<nplanets; i++) {
-//       nextplanets[i].vx = planets[i].vx;
-//       nextplanets[i].vy = planets[i].vy;
-//       nextplanets[i].mass = planets[i].mass;
-//       nextplanets[i].x = planets[i].x;
-//       nextplanets[i].y = planets[i].y;
-//    }
-
-//    #pragma omp parallel for simd
-//    for (int i=0; i<nplanets; i++) {
-//       for (int j=0; j<nplanets; j++) {
-//          double dx = planets[j].x - planets[i].x;
-//          double dy = planets[j].y - planets[i].y;
-//          double distSqr = dx*dx + dy*dy + 0.0001;
-//          double invDist = planets[i].mass * planets[j].mass / sqrt(distSqr);
-//          double invDist3 = invDist * invDist * invDist;
-//          double val = dt * invDist3;
-//          nextplanets[i].vx += dx * val;
-//          nextplanets[i].vy += dy * val;
-//       }
-//       nextplanets[i].x += dt * nextplanets[i].vx;
-//       nextplanets[i].y += dt * nextplanets[i].vy;
-//    }
-//    free(planets);
-//    return nextplanets;
-// }
-
-// PlanetArray next(PlanetArray planets) {
-//    // Set up nextplanets SoA
-//    PlanetArray nextplanets;
-//    nextplanets.mass = (double*)malloc(sizeof(double) * nplanets);
-//    nextplanets.x = (double*)malloc(sizeof(double) * nplanets);
-//    nextplanets.y = (double*)malloc(sizeof(double) * nplanets);
-//    nextplanets.vx = (double*)malloc(sizeof(double) * nplanets);
-//    nextplanets.vy = (double*)malloc(sizeof(double) * nplanets);
-
-//    #pragma omp parallel for
-//    for (int i=0; i<nplanets; i++) {
-//       nextplanets.vx[i] = planets.vx[i];
-//       nextplanets.vy[i] = planets.vy[i];
-//       nextplanets.mass[i] = planets.mass[i];
-//       nextplanets.x[i] = planets.x[i];
-//       nextplanets.y[i] = planets.y[i];
-
-//       for (int j=0; j<nplanets; j++) {
-//          double dx = planets.x[j] - planets.x[i];
-//          double dy = planets.y[j] - planets.y[i];
-//          double distSqr = dx*dx + dy*dy + 0.0001;
-//          double invDist = planets.mass[i] * planets.mass[j] / sqrt(distSqr);
-//          double invDist3 = invDist * invDist * invDist;
-//          double val = dt * invDist3;
-//          nextplanets.vx[i] += dx * val;
-//          nextplanets.vy[i] += dy * val;
-//       }
-//       nextplanets.x[i] += dt * nextplanets.vx[i];
-//       nextplanets.y[i] += dt * nextplanets.vy[i];
-//    }
-      
-//    free(planets.mass);
-//    free(planets.x);
-//    free(planets.y);
-//    free(planets.vx);
-//    free(planets.vy);
-//    return nextplanets;
-// }
-
 void next(const PlanetArray* planets, PlanetArray* nextplanets) {
-   #pragma omp parallel for simd
-   for (int i=0; i<nplanets; i++) {
-      nextplanets->vx[i] = planets->vx[i];
-      nextplanets->vy[i] = planets->vy[i];
-      nextplanets->mass[i] = planets->mass[i];
-      nextplanets->x[i] = planets->x[i];
-      nextplanets->y[i] = planets->y[i];
+   #pragma omp parallel for
+   for (int i = 0; i < nplanets; i++) {
+      double xi = planets->x[i];
+      double yi = planets->y[i];
+      double mi = planets->mass[i];
+      double vxi = planets->vx[i];
+      double vyi = planets->vy[i];
 
-      for (int j=0; j<nplanets; j++) {
-         double dx = planets->x[j] - planets->x[i];
-         double dy = planets->y[j] - planets->y[i];
-         double distSqr = dx*dx + dy*dy + 0.0001;
-         double invDist = planets->mass[i] * planets->mass[j] / sqrt(distSqr);
-         double invDist3 = invDist * invDist * invDist;
-         double val = dt * invDist3;
-         nextplanets->vx[i] += dx * val;
-         nextplanets->vy[i] += dy * val;
+      // broadcasting into SIMD
+      __m256d xi_vec = _mm256_set1_pd(xi);
+      __m256d yi_vec = _mm256_set1_pd(yi);
+      __m256d mi_vec = _mm256_set1_pd(mi);
+      __m256d dt_vec = _mm256_set1_pd(dt);
+      __m256d fx_vec = _mm256_setzero_pd();
+      __m256d fy_vec = _mm256_setzero_pd();
+
+      double fx = 0.0;
+      double fy = 0.0;
+
+      int j;
+      for (j = 0; j <= nplanets - 4; j += 4) {
+         // loading from memory
+         __m256d xj = _mm256_loadu_pd(&planets->x[j]);
+         __m256d yj = _mm256_loadu_pd(&planets->y[j]);
+         __m256d mj = _mm256_loadu_pd(&planets->mass[j]);
+
+         __m256d dx = _mm256_sub_pd(xj, xi_vec);
+         __m256d dy = _mm256_sub_pd(yj, yi_vec);
+         __m256d dx2 = _mm256_mul_pd(dx, dx);
+         __m256d dy2 = _mm256_mul_pd(dy, dy);
+         __m256d distSqr = _mm256_add_pd(_mm256_add_pd(dx2, dy2), _mm256_set1_pd(0.0001));
+         __m256d dist = _mm256_sqrt_pd(distSqr);
+
+         __m256d mprod = _mm256_mul_pd(mi_vec, mj);
+         __m256d dist3 = _mm256_mul_pd(dist, _mm256_mul_pd(dist, dist));
+         __m256d inv = _mm256_div_pd(mprod, dist3);
+         __m256d val = _mm256_mul_pd(dt_vec, inv);
+
+         // same as fx += dx * val
+         fx_vec = _mm256_fmadd_pd(dx, val, fx_vec);
+         fy_vec = _mm256_fmadd_pd(dy, val, fy_vec);
       }
-      nextplanets->x[i] += dt * nextplanets->vx[i];
-      nextplanets->y[i] += dt * nextplanets->vy[i];
+
+      // get fx and fy from 4-element vectors
+      double fx_arr[4], fy_arr[4];
+      _mm256_storeu_pd(fx_arr, fx_vec);
+      _mm256_storeu_pd(fy_arr, fy_vec);
+      fx = fx_arr[0] + fx_arr[1] + fx_arr[2] + fx_arr[3];
+      fy = fy_arr[0] + fy_arr[1] + fy_arr[2] + fy_arr[3];
+
+      // handling tail case
+      for (; j < nplanets; j++) {
+         double dx = planets->x[j] - xi;
+         double dy = planets->y[j] - yi;
+         double distSqr = dx*dx + dy*dy + 0.0001;
+         double dist = sqrt(distSqr);
+         double val = dt * mi * planets->mass[j] / (dist * dist * dist);
+         fx += dx * val;
+         fy += dy * val;
+      }
+
+      double nvx = vxi + fx;
+      double nvy = vyi + fy;
+      nextplanets->vx[i] = nvx;
+      nextplanets->vy[i] = nvy;
+      nextplanets->x[i] = xi + dt * nvx;
+      nextplanets->y[i] = yi + dt * nvy;
+      nextplanets->mass[i] = mi;
    }
 }
+
 
 int main(int argc, const char** argv){
    if (argc < 2) {
