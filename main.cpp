4aabd88e977eedf78e546e11ed30358b83204130
divyack2 <divyack2@sp25-cs598a-08.cs.illinois.edu>
2025-04-14 20:03:44 -0500

    adding a serial_next() for small n

--- main.cpp
+++ main.cpp
@@ -40,6 +40,84 @@ int timesteps;
 double dt;
 double G;
 
+void serial_next(const PlanetArray* planets) {
+   // pull out constants
+   __m256d dt_vec = _mm256_set1_pd(dt);
+   __m256d e = _mm256_set1_pd(0.0001);
+
+   for (int i = 0; i < nplanets; i++) {
+      double xi = planets->x[i];
+      double yi = planets->y[i];
+      double mi = planets->mass[i];
+      double vxi = planets->vx[i];
+      double vyi = planets->vy[i];
+
+      // broadcasting into SIMD
+      __m256d xi_vec = _mm256_set1_pd(xi);
+      __m256d yi_vec = _mm256_set1_pd(yi);
+      __m256d mi_vec = _mm256_set1_pd(mi);
+      __m256d fx_vec = _mm256_setzero_pd();
+      __m256d fy_vec = _mm256_setzero_pd();
+
+      double fx = 0.0;
+      double fy = 0.0;
+
+      int j;
+      for (j = 0; j <= nplanets - 4; j += 4) {
+         // loading from memory
+         __m256d xj = _mm256_loadu_pd(&planets->x[j]);
+         __m256d yj = _mm256_loadu_pd(&planets->y[j]);
+         __m256d mj = _mm256_loadu_pd(&planets->mass[j]);
+
+         __m256d dx = _mm256_sub_pd(xj, xi_vec);
+         __m256d dy = _mm256_sub_pd(yj, yi_vec);
+         __m256d dx2 = _mm256_mul_pd(dx, dx);
+         __m256d dy2 = _mm256_mul_pd(dy, dy);
+         __m256d distSqr = _mm256_add_pd(_mm256_add_pd(dx2, dy2), e);
+         __m256d dist = _mm256_rsqrt14_pd(distSqr);
+
+         __m256d mprod = _mm256_mul_pd(mi_vec, mj);
+         __m256d dist3 = _mm256_mul_pd(dist, _mm256_mul_pd(dist, dist));
+         __m256d inv = _mm256_mul_pd(mprod, dist3);
+         __m256d val = _mm256_mul_pd(dt_vec, inv);
+
+         // same as fx += dx * val
+         fx_vec = _mm256_fmadd_pd(dx, val, fx_vec);
+         fy_vec = _mm256_fmadd_pd(dy, val, fy_vec);
+      }
+
+      // get fx and fy from 4-element vectors
+      double fx_arr[4], fy_arr[4];
+      _mm256_storeu_pd(fx_arr, fx_vec);
+      _mm256_storeu_pd(fy_arr, fy_vec);
+      fx = fx_arr[0] + fx_arr[1] + fx_arr[2] + fx_arr[3];
+      fy = fy_arr[0] + fy_arr[1] + fy_arr[2] + fy_arr[3];
+
+      // handling tail case
+      for (; j < nplanets; j++) {
+         double dx = planets->x[j] - xi;
+         double dy = planets->y[j] - yi;
+         double distSqr = dx*dx + dy*dy + 0.0001;
+         double dist = sqrt(distSqr);
+         double val = dt * mi * planets->mass[j] / (dist * dist * dist);
+         fx += dx * val;
+         fy += dy * val;
+      }
+
+      double nvx = vxi + fx;
+      double nvy = vyi + fy;
+      planets->vx[i] = nvx;
+      planets->vy[i] = nvy;
+   }
+
+   // modify positions in-place after all velocities have been updated
+   #pragma omp simd
+   for (int i = 0; i < nplanets; i++) {
+      planets->x[i] += dt * planets->vx[i];
+      planets->y[i] += dt * planets->vy[i];
+   }
+}
+
 void next(const PlanetArray* planets) {
    // pull out constants
    __m256d dt_vec = _mm256_set1_pd(dt);
@@ -149,7 +227,11 @@ int main(int argc, const char** argv){
    struct timeval start, end;
    gettimeofday(&start, NULL);
    for (int i=0; i<timesteps; i++) {
-      next(&planets);
+      if (nplanets > 50) {
+         next(&planets);
+      } else {
+         serial_next(&planets);
+      }
    }
    gettimeofday(&end, NULL);
    printf("Total time to run simulation %0.6f seconds, final location %f %f\n", tdiff(&start, &end), planets.x[nplanets-1], planets.y[nplanets-1]);
